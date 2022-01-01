3f257196ff633a9fa04f1de81cdef7e0a22fa79a
J-45 <64985480+J-45@users.noreply.github.com>
2022-01-01 22:27:37 +0100

    Add files via upload

--- App.java
+++ App.java
@@ -1,20 +1,70 @@
-import java.util.Arrays;
-import java.util.regex.*;
+// import java.util.Arrays;
+// import java.util.regex.*;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
-        String[] urls = keepYoutubeUrlsOnly(args);
+    public static void main(String[] urls) throws Exception {
+        // System.out.println(Arrays.toString(urls));
         
-        for (int index = 0; index < args.length; ++index)
-        {
-            System.out.println("args[" + index + "]: " + args[index]);
+        String youtubeUrlRegex = "https?://(www\\.)?(youtube\\.com|youtu\\.be)/watch\\?v=[\\w&=]+";
+        String[] youtubeUrls = keepYoutubeUrlsOnly(youtubeUrlRegex, urls);
+
+        for (int youtubeUrlsLoop = 0; youtubeUrlsLoop < youtubeUrls.length; ++youtubeUrlsLoop)
+        {   
+            downloadVideo(youtubeUrls[youtubeUrlsLoop]);
         }
-        
     }
 
-    public static String[] keepYoutubeUrlsOnly(String[] args) {
-        System.out.println(Arrays.toString(args));
-        return args;
+    public static String[] keepYoutubeUrlsOnly(String youtubeUrlRegex, String[] urls) {
+        int numberOfValidUrls = countValidUrls(youtubeUrlRegex, urls);
+        int addedUrl = 0;
+        String[] youtubeUrls = new String[numberOfValidUrls];
+
+        for (int index = 0; index < urls.length; ++index)
+        {   
+            String url = urls[index];
+            if (url.matches(youtubeUrlRegex))
+            {
+                youtubeUrls[addedUrl] = url;
+                addedUrl++;
+            }
+        }
+        return youtubeUrls;
+    }
+
+    public static int countValidUrls(String youtubeUrlRegex,String[] args) {
+        int goodUrlsCounter = 0;
+        for (int index = 0; index < args.length; ++index)
+        {   
+            String url = args[index];
+            if (url.matches(youtubeUrlRegex)){
+                // System.out.println("urls[" + index + "]: " + url);
+                goodUrlsCounter++;
+            }
+        }
+        return goodUrlsCounter;
+    }
+
+    public static String[] getUrlsFromplaylist(String url) {
+        String[] foundUrls = {""};
+        return foundUrls;
+    }
+
+    public static boolean downloadVideo(String url) {
+        String youtubePlaylistUrlRegex = "https?://(www\\.)?(youtube\\.com|youtu\\.be)/watch\\?v=\\w+&list=\\w+";
+
+        if (url.matches(youtubePlaylistUrlRegex)){
+            System.out.println("PLAYLIST: " + url);
+            String[] playlistUrls = getUrlsFromplaylist(url);
+            for (int playlistLoopCount = 0; playlistLoopCount < playlistUrls.length; ++playlistLoopCount)
+            {   
+                String singlePlaylistUrl = playlistUrls[playlistLoopCount];
+                downloadVideo(singlePlaylistUrl);
+            }
+        }
+        else{
+            System.out.println("URL: " + url);
+        }
+        return true;
     }
 }
--- AppTest.java
+++ AppTest.java
@@ -1,16 +1,26 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
-import java.util.regex.*;
-import java.util.Arrays;
+// import java.util.regex.*;
+// import java.util.Arrays;
 
 public class AppTest {
 
-    String[] args = {"","https://stackoverflow.com/","https://www.youtube.com/watch?v=3PcIJKd1PKU", "https://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&start_radio=1&rv=wjAdxAbmQNM&t=0"};
+    String[] testUrls = {"","https://stackoverflow.com/","https://www.youtube.com/watch?v=3PcIJKd1PKU", "https://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM","https://youtu.be/watch?v=3PcIJKd1PKU","http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
+
+    String[] goodUrls = {"https://www.youtube.com/watch?v=3PcIJKd1PKU", "https://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM","https://youtu.be/watch?v=3PcIJKd1PKU","http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
+
+    String youtubeUrlRegex = "https?://(www\\.)?(youtube\\.com|youtu\\.be)/watch\\?v=[\\w&=]+";
 
     @Test
-    @DisplayName("get Url In Args test")
+    @DisplayName("get Url In Arg")
     void keepYoutubeUrlsOnly_Test() {
-        assertEquals(args, App.keepYoutubeUrlsOnly(args));
+        assertArrayEquals(goodUrls, App.keepYoutubeUrlsOnly(youtubeUrlRegex, testUrls));
+    }
+
+    @Test
+    @DisplayName("count valid url in args with regex")
+    void countValidUrls_Test() {
+        assertEquals(4, App.countValidUrls(youtubeUrlRegex, testUrls));
     }
 
 }
