b1b7810f3c8c8ed4cc4b6c3cf3554b5a8176a01e
J-45 <64985480+J-45@users.noreply.github.com>
2022-01-06 11:59:19 +0100

    Update AppTest.java

--- AppTest.java
+++ AppTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.*;
 import java.io.IOException;
 // import java.util.regex.*;
@@ -6,8 +8,8 @@ import java.io.IOException;
 
 public class AppTest {
 
-    String[] testUrls = {"","https://stackoverflow.com/", "https://www.youtube.com/watch?v=_SnGxw13QxU", "https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m","https://www.youtube.com/watch?v=3PcIJKd1PKU","https://youtu.be/watch?v=3PcIJKd1PKU","http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
-    String[] goodUrls = {"https://www.youtube.com/watch?v=_SnGxw13QxU", "https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m","https://www.youtube.com/watch?v=3PcIJKd1PKU","https://youtu.be/watch?v=3PcIJKd1PKU","http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
+    String[] testUrls = {"","https://stackoverflow.com/","https://www.youtube.com/watch?v=iKBs9l8jS6Q", "https://www.youtube.com/watch?v=_SnGxw13QxU", "https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m","https://www.youtube.com/watch?v=3PcIJKd1PKU","https://youtu.be/watch?v=3PcIJKd1PKU","http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
+    String[] goodUrls = {"https://www.youtube.com/watch?v=iKBs9l8jS6Q", "https://www.youtube.com/watch?v=_SnGxw13QxU", "https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m","https://www.youtube.com/watch?v=3PcIJKd1PKU","https://youtu.be/watch?v=3PcIJKd1PKU","http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
     String[] goodPlaylistUrls = {"https://www.youtube.com/watch?v=wfWxdh-_k_4&list=PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx","https://www.youtube.com/watch?v=C5cnZ-gZy2I&list=PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx"};
     String youtubeUrlRegex = "https?://(www\\.)?(youtube\\.com|youtu\\.be)/watch\\?v=[\\w&=]+";
 
@@ -45,5 +47,22 @@ public class AppTest {
     @DisplayName("get playlist id from playlist url")
     void getPlayslistId_Test() {
         assertEquals("PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx", App.getPlayslistId("https://www.youtube.com/watch?v=wfWxdh-_k_4&list=PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx"));
+    }
+
+    @Test
+    @DisplayName("get download url")
+    void getDownloadUrl_Test() throws IOException, Exception {
+        String[] audioAndVideo = new String[2];
+        audioAndVideo = App.getDownloadUrl("https://www.youtube.com/watch?v=iKBs9l8jS6Q");
+        Assert.assertTrue(audioAndVideo[0].contains("videoplayback"));
+        Assert.assertTrue(audioAndVideo[0].contains("videoplayback"));
+    }
+
+    @Test
+    @DisplayName("download url")
+    void download_Test() throws IOException, Exception {
+        String[] audioAndVideo = new String[2];
+        audioAndVideo = App.getDownloadUrl("https://www.youtube.com/watch?v=iKBs9l8jS6Q");
+        App.download(audioAndVideo[0],"video.mp4");
     }
 }
