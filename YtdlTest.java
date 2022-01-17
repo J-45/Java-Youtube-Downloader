3f7129388edc05b163619867e3a297dc2d661b6a
J-45 <64985480+J-45@users.noreply.github.com>
2022-01-17 11:28:21 +0100

    Update YtdlTest.java

--- YtdlTest.java
+++ YtdlTest.java
@@ -7,30 +7,37 @@ import java.io.IOException;
 // import java.util.regex.*;
 // import java.util.Arrays;
 
-public class Ytdl_Test {
+public class YtdlTest {
 
-    String[] testUrls = {"","https://stackoverflow.com/", "https://www.youtube.com/watch?v=_SnGxw13QxU", "https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m","https://youtu.be/watch?v=3PcIJKd1PKU","http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
-    String[] goodUrls = { "https://www.youtube.com/watch?v=_SnGxw13QxU", "https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m","http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
+    String[] testUrls = {"","https://stackoverflow.com/",
+    		"https://www.youtube.com/watch?v=_SnGxw13QxU",
+    		"https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m",
+    		"https://youtu.be/watch?v=3PcIJKd1PKU",
+    		"http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
+    String[] goodUrls = {"https://www.youtube.com/watch?v=_SnGxw13QxU", 
+    		"https://www.youtube.com/watch?v=KJgsSFOSQv0&list=PLWKjhJtqVAbmUE5IqyfGYEYjrZBYzaT4m",
+    		"https://youtu.be/watch?v=3PcIJKd1PKU",
+    		"http://www.youtube.com/watch?v=wjAdxAbmQNM&list=RDwjAdxAbmQNM&=8ds1v887s8d7f8d"};
     String[] goodPlaylistUrls = {"https://www.youtube.com/watch?v=wfWxdh-_k_4&list=PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx","https://www.youtube.com/watch?v=C5cnZ-gZy2I&list=PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx"};
     String youtubeUrlRegex = "https?://(www\\.)?(youtube\\.com|youtu\\.be)/watch\\?v=[\\w&=]+";
+    String[] testMain = {"https://www.youtube.com/watch?v=3PcIJKd1PKU"};
 
     @Test
     @DisplayName("test main")
     void main_Test()  throws Exception{
-        String[] testMain = {"https://www.youtube.com/watch?v=3PcIJKd1PKU"};
-        Ytdl.main(testMain);
+        Ytdl.main(this.testMain);
     }
 
     @Test
     @DisplayName("get Url In Arg")
     void keepYoutubeUrlsOnly_Test() {
-        assertArrayEquals(goodUrls, Ytdl.keepYoutubeUrlsOnly(youtubeUrlRegex, testUrls));
+        assertArrayEquals(this.goodUrls, Ytdl.keepYoutubeUrlsOnly(this.youtubeUrlRegex, this.testUrls));
     }
 
     @Test
     @DisplayName("count valid url in args with regex")
     void countValidUrls_Test() {
-        assertEquals(testUrls.length - 2, Ytdl.countValidUrls(youtubeUrlRegex, testUrls));
+        assertEquals(this.testUrls.length - 2, Ytdl.countValidUrls(this.youtubeUrlRegex, this.testUrls));
     }
 
     @Test
@@ -42,13 +49,13 @@ public class Ytdl_Test {
     @Test
     @DisplayName("get urls from html content")
     void getUrlsFromplaylist_Test() throws IOException, InterruptedException {
-        assertArrayEquals(goodPlaylistUrls, Ytdl.getUrlsFromplaylist("https://www.youtube.com/watch?v=wfWxdh-_k_4&list=PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx"));
+        assertArrayEquals(this.goodPlaylistUrls, Ytdl.getUrlsFromplaylist(this.goodPlaylistUrls[0]));
     }
 
     @Test
     @DisplayName("get playlist id from playlist url")
     void getPlayslistId_Test() {
-        assertEquals("PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx", Ytdl.getPlayslistId("https://www.youtube.com/watch?v=wfWxdh-_k_4&list=PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx"));
+        assertEquals("PLWKjhJtqVAbkq5Oh8ERRJ1aPZK2NKBSRx", Ytdl.getPlayslistId(this.goodPlaylistUrls[0]));
     }
 
     @Test
@@ -61,6 +68,7 @@ public class Ytdl_Test {
         String title = data[2];
         int videoContentLength = Integer.parseInt(data[3]);
         int audioContentLength = Integer.parseInt(data[4]);
+        System.out.println(data);
         Assert.assertEquals(videoContentLength, 1122164703);
         Assert.assertEquals(audioContentLength, 11330218);
         Assert.assertTrue(videoLink.contains("videoplayback"));
@@ -84,16 +92,17 @@ public class Ytdl_Test {
     @Test
     @DisplayName("join audio and video")
     void join_Test() throws IOException, Exception {
+        // prep
         String[] data = new String[5];
         data = Ytdl.getData("https://www.youtube.com/watch?v=3PcIJKd1PKU");
         String videoLink = data[0];
         String audioLink = data[1];
+        String title = data[2];
         int videoContentLength = Integer.parseInt(data[3]);
         int audioContentLength = Integer.parseInt(data[4]);
         Ytdl.download(videoLink,"video.mp4",videoContentLength);
         Ytdl.download(audioLink,"audio.mp3",audioContentLength);
-        String title = data[2];
-        
+        // test
         Assert.assertTrue(Ytdl.join(title+".mp4"));
     }
 }
